ffeb58ce1fa8704ecb93c7defe06b283304f1f60
Pravin  Mishra <diatm.pravin.it.07.27@gmail.com>
2012-05-21 07:52:01 +0530

    first commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterBootstrapRails3Demo::Application.config.secret_token = '42c1070a536c55c4ea2aaa2612b96e9a0bbb68f6654f2d06c1a2694f83c219995fde6efac3afda89158be4aff8b4d7c929065f1ed0bd1541e23fc0984aa2c50f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterBootstrapRails3Demo::Application.config.session_store :cookie_store, key: '_twitter-bootstrap-rails-3-demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TwitterBootstrapRails3Demo::Application.config.session_store :active_record_store
